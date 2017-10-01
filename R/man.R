54c07dfe96461fa5d8b45e2ef88254127e0e9d9b
Michael Friendly <friendly@yorku.ca>
2017-10-01 11:04:13 -0400

    move man to its own .R file

---
+++ R/man.R
@@ -0,0 +1,23 @@
+#' Find and display documentation for a package
+
+#' Opens the CRAN documentation for a package or builds it from the local R library
+
+#' @param package  name of a package, with or without quotes
+#' @param method	either \code{"web"} or \code{"system"}.  \code{"web"} uses the manual from CRAN.
+#'                \code{"system"}  builds the documentation from your local R version.
+#' @export
+
+
+man <- function(package, method=c("system", "web")){
+  
+  LIB <- substitute(package)
+  LIB <- as.character(LIB)
+  method <- match.arg(method)
+  
+  switch(method,
+         web = browseURL(paste("https://cran.r-project.org/web/packages/",LIB,"/",LIB,".pdf", sep = "")),
+         system =  {unlink(paste(getwd(),"/",LIB,".pdf",sep=""))
+           path <- find.package(LIB)
+           system(paste(shQuote(file.path(R.home("bin"), "R")),"CMD", "Rd2pdf",shQuote(path)))})   
+}
+
--- R/myutil.R
+++ R/myutil.R
@@ -70,27 +70,6 @@ lsall  <-  function(..., all.names = TRUE) {
 	    postscript(file=file, onefile=FALSE, horizontal=horizontal, paper=paper, height=height, width=width,...)
 	  }
 
-#' Find and display documentation for a package
-
-#' Opens the CRAN documentation for a package or builds it from the local R library
-
-#' @param package  name of a package, with or without quotes
-#' @param method	either \code{"web"} or \code{"system"}
-#' @export
-
-
-man <- function(package, method=c("web", "system")){
-  
-  LIB <- substitute(package)
-  LIB <- as.character(LIB)
-  method <- match.arg(method)
-  
-  switch(method,
-         web = browseURL(paste("http://cran.r-project.org/web/packages/",LIB,"/",LIB,".pdf", sep = "")),
-         system =  {unlink(paste(getwd(),"/",LIB,".pdf",sep=""))
-                    path <- find.package(LIB)
-                    system(paste(shQuote(file.path(R.home("bin"), "R")),"CMD", "Rd2pdf",shQuote(path)))})   
-}
 
 #' Set nicer default par()
 #' 
